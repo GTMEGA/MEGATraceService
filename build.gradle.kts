a57838fcd8634e6d9cf8db43b5dc11f389cbb83e
Ven <tudurap.com@gmail.com>
2025-01-19 15:41:16 +0000

    Publication stubs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,25 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "mega.trace"
 version = "1.0.0"
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+
+tasks.jar {
+    archiveBaseName.set("megatraceservice")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "megatraceservice"
+            from(components["java"])
+        }
+    }
+    repositories {
+        mavenLocal() //TODO: Actual maven publish
+    }
+}
